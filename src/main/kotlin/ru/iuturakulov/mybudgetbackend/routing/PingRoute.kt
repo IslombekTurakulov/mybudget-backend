3b6bbb7a8caef7a89f8fb795f75bb955e87d3219
iuturakulov <me@turakulov.ru>
2025-05-09 06:54:54 +0300

    MYBUDGETBACKEND-74: fix migration file

--- src/main/kotlin/ru/iuturakulov/mybudgetbackend/routing/PingRoute.kt
+++ src/main/kotlin/ru/iuturakulov/mybudgetbackend/routing/PingRoute.kt
@@ -2,13 +2,18 @@ package ru.iuturakulov.mybudgetbackend.routing
 
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("PingRoute")
 
 fun Route.pingRoute() {
     head("/ping") {
+        logger.info("Received HEAD request to /ping")
         call.respondText("") // Пустое тело ответа для HEAD запроса
     }
 
     get("/ping") {
+        logger.info("Received GET request to /ping")
         call.respondText("pong") // Для тестирования
     }
 }
