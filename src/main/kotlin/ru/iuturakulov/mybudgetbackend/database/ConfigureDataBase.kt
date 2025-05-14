e63c2c88c1ee3bfd335e188c6674f6ed85c26c20
iuturakulov <me@turakulov.ru>
2025-05-14 07:03:48 +0300

    MYBUDGETBACKEND-93: add missing schema creation, update readme

--- src/main/kotlin/ru/iuturakulov/mybudgetbackend/database/ConfigureDataBase.kt
+++ src/main/kotlin/ru/iuturakulov/mybudgetbackend/database/ConfigureDataBase.kt
@@ -6,6 +6,19 @@ import io.ktor.server.config.*
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.FlywayException
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import ru.iuturakulov.mybudgetbackend.entities.*
+import ru.iuturakulov.mybudgetbackend.entities.audit.AuditLogTable
+import ru.iuturakulov.mybudgetbackend.entities.fcm.DeviceTokens
+import ru.iuturakulov.mybudgetbackend.entities.invitation.InvitationTable
+import ru.iuturakulov.mybudgetbackend.entities.notification.FCMNotificationTable
+import ru.iuturakulov.mybudgetbackend.entities.notification.NotificationTable
+import ru.iuturakulov.mybudgetbackend.entities.participants.ParticipantTable
+import ru.iuturakulov.mybudgetbackend.entities.projects.ProjectsTable
+import ru.iuturakulov.mybudgetbackend.entities.transaction.TransactionsTable
+import ru.iuturakulov.mybudgetbackend.entities.user.EmailVerificationTable
+import ru.iuturakulov.mybudgetbackend.entities.user.UserTable
 import javax.sql.DataSource
 
 fun configureDatabase(config: ApplicationConfig) {
@@ -43,8 +56,24 @@ object DatabaseConfig {
             leakDetectionThreshold = 60000
         }
         val dataSource = HikariDataSource(hikariConfig)
-//        runFlyway(dataSource)
+        // runFlyway(dataSource)
         Database.connect(dataSource)
+        
+        // Автоматическое создание таблиц
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(
+                UserTable,
+                ProjectsTable,
+                ParticipantTable,
+                TransactionsTable,
+                NotificationTable,
+                AuditLogTable,
+                FCMNotificationTable,
+                DeviceTokens,
+                EmailVerificationTable,
+                InvitationTable
+            )
+        }
     }
 
     private fun runFlyway(dataSource: DataSource) {
